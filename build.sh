7663c193f69e1e005433b9215852365fb8c922db
Robert Nelson <robertcnelson@gmail.com>
2022-01-05 12:24:03 -0600

    build.sh: build on target
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sys=$(uname -m)
+
 #add: bc bison flex libssl-dev u-boot-tools python3-pycryptodome python3-pyelftools
 #binutils-arm-linux-gnueabihf binutils-aarch64-linux-gnu
 #gcc-arm-linux-gnueabihf gcc-aarch64-linux-gnu
@@ -7,20 +9,39 @@
 DIR="$PWD"
 
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- j721e_evm_r5_defconfig
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
 
-make ARCH=arm -j4 CROSS_COMPILE=arm-linux-gnueabihf-
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- savedefconfig
-cp -v defconfig ./configs/j721e_evm_r5_defconfig
-ls -lh ./tiboot3.bin
+if [ "x${sys}" = "xaarch64" ] ; then
+	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- j721e_evm_r5_defconfig O=../r5
+	make ARCH=arm -j2 CROSS_COMPILE=arm-linux-gnueabihf- O=../r5
+
+	cp -v ../r5/tiboot3.bin /opt/u-boot/bb-u-boot-beagleboneai64
+else
+	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- j721e_evm_r5_defconfig
+	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
+	make ARCH=arm -j4 CROSS_COMPILE=arm-linux-gnueabihf-
+
+	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- savedefconfig
+	cp -v defconfig ./configs/j721e_evm_r5_defconfig
+
+	ls -lh ./tiboot3.bin
+fi
 
 make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- distclean
-make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- j721e_evm_a72_defconfig
-make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- menuconfig
 
-make ARCH=arm -j4 CROSS_COMPILE=aarch64-linux-gnu- ATF=${DIR}/ti-blobs/bl31.bin TEE=${DIR}/ti-blobs/tee-pager_v2.bin DM=${DIR}/ti-blobs/ipc_echo_testb_mcu1_0_release_strip.xer5f
-make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- savedefconfig
-cp -v defconfig ./configs/j721e_evm_a72_defconfig
-ls -lh ./tispl.bin
-ls -lh ./u-boot.bin
+if [ "x${sys}" = "xaarch64" ] ; then
+	make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- j721e_evm_a72_defconfig O=../a72
+	make -j2 CROSS_COMPILE=aarch64-linux-gnu- ATF=/opt/u-boot/bb-arm-trusted-firmware-k3-generic-opteed/bl31.bin TEE=/opt/u-boot/bb-optee-os-k3-j721e/tee-pager_v2.bin DM=/opt/u-boot/bb-u-boot-beagleboneai64/ipc_echo_testb_mcu1_0_release_strip.xer5f O=../a72
+
+	cp -v ../a72/tispl.bin /opt/u-boot/bb-u-boot-beagleboneai64
+	cp -v ../a72/u-boot.img /opt/u-boot/bb-u-boot-beagleboneai64
+else
+	make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- j721e_evm_a72_defconfig
+	make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- menuconfig
+	make ARCH=arm -j4 CROSS_COMPILE=aarch64-linux-gnu- ATF=${DIR}/ti-blobs/bl31.bin TEE=${DIR}/ti-blobs/tee-pager_v2.bin DM=${DIR}/ti-blobs/ipc_echo_testb_mcu1_0_release_strip.xer5f
+
+	make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- savedefconfig
+	cp -v defconfig ./configs/j721e_evm_a72_defconfig
+
+	ls -lh ./tispl.bin
+	ls -lh ./u-boot.bin
+fi
